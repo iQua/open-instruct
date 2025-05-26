d0fda65eab3cdd3baa79e1592977dc5c6f278f1e
Fei Wang <silviafey.wang@utoronto.ca>
2025-05-26 21:29:20 +0000

    Created a script to fine-tune OLMoE with LoRA using open-instruct.

---
+++ scripts/finetune_moe.sh
@@ -0,0 +1,35 @@
+export CUDA_VISIBLE_DEVICES=0
+
+accelerate launch \
+--mixed_precision bf16 \
+--dynamo_backend inductor \
+--num_machines 1 \
+--num_processes 8 \
+--use_deepspeed \
+--deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
+open_instruct/finetune.py \
+--model_name_or_path allenai/OLMoE-1B-7B-0924 \
+--tokenizer_name allenai/OLMoE-1B-7B-0924 \
+--use_flash_attn \
+--use_lora \
+--lora_rank 64 \
+--lora_alpha 16 \
+--lora_dropout 0.1 \
+--max_seq_length 4096 \
+--preprocessing_num_workers 128 \
+--per_device_train_batch_size 2 \
+--gradient_accumulation_steps 8 \
+--learning_rate 2e-05 \
+--lr_scheduler_type linear \
+--warmup_ratio 0.03 \
+--weight_decay 0.0 \
+--num_train_epochs 2 \
+--output_dir output/ \
+--with_tracking \
+--report_to wandb \
+--logging_steps 1 \
+--reduce_loss sum \
+--model_revision main \
+--dataset_mixer_list allenai/tulu-v3-mix-preview-4096-OLMoE 1.0 ai2-adapt-dev/daring-anteater-specialized 1.0 \
+--checkpointing_steps epoch \
+--add_bos
