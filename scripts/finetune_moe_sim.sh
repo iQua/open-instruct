10b221cc705f992566faf854614ae674a78bdb0b
Fei Wang <silviafey.wang@utoronto.ca>
2025-05-28 16:48:38 -0400

    Added a fine-tuning script to run on sim.

--- scripts/finetune_moe_sim.sh
+++ scripts/finetune_moe_sim.sh
@@ -1,11 +1,11 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 accelerate launch \
 --mixed_precision bf16 \
 --dynamo_backend inductor \
 --num_machines 1 \
---num_processes 1 \
+--num_processes 3 \
 --use_deepspeed \
 --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
 open_instruct/finetune.py \
@@ -13,11 +13,11 @@ open_instruct/finetune.py \
 --tokenizer_name allenai/OLMoE-1B-7B-0924 \
 --use_flash_attn \
 --use_lora \
---lora_rank 64 \
+--lora_rank 32 \
 --lora_alpha 16 \
 --lora_dropout 0.1 \
---max_seq_length 4096 \
---preprocessing_num_workers 128 \
+--max_seq_length 2048 \
+--preprocessing_num_workers 8 \
 --per_device_train_batch_size 1 \
 --gradient_accumulation_steps 16 \
 --learning_rate 2e-05 \
@@ -26,11 +26,10 @@ open_instruct/finetune.py \
 --weight_decay 0.0 \
 --num_train_epochs 2 \
 --output_dir output/ \
---with_tracking \
 --logging_steps 1 \
 --reduce_loss sum \
 --model_revision main \
---dataset_mixer_list allenai/tulu-v3.1-mix-preview-4096-OLMoE 1.0 \
+--dataset_mixer_list allenai/tulu-v3.1-mix-preview-4096-OLMoE 0.1 \
 --checkpointing_steps epoch \
 --add_bos \
 --gradient_checkpointing
