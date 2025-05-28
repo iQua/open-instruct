8d27a11c8c661548e7486280af25865cef60dd6b
Fei Wang <silviafey.wang@utoronto.ca>
2025-05-28 20:45:36 +0000

    Adjusted fine-tuning configs to reduce memory usage.

--- scripts/finetune_moe_boston.sh
+++ scripts/finetune_moe_boston.sh
@@ -6,18 +6,16 @@ accelerate launch \
 --dynamo_backend inductor \
 --num_machines 1 \
 --num_processes 1 \
---use_deepspeed \
---deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
 open_instruct/finetune.py \
 --model_name_or_path allenai/OLMoE-1B-7B-0924 \
 --tokenizer_name allenai/OLMoE-1B-7B-0924 \
 --use_flash_attn \
 --use_lora \
---lora_rank 64 \
+--lora_rank 32 \
 --lora_alpha 16 \
 --lora_dropout 0.1 \
---max_seq_length 4096 \
---preprocessing_num_workers 128 \
+--max_seq_length 1024 \
+--preprocessing_num_workers 4 \
 --per_device_train_batch_size 1 \
 --gradient_accumulation_steps 16 \
 --learning_rate 2e-05 \
@@ -26,11 +24,10 @@ open_instruct/finetune.py \
 --weight_decay 0.0 \
 --num_train_epochs 2 \
 --output_dir output/ \
---with_tracking \
 --logging_steps 1 \
 --reduce_loss sum \
 --model_revision main \
---dataset_mixer_list allenai/tulu-v3.1-mix-preview-4096-OLMoE 1.0 \
+--dataset_mixer_list allenai/tulu-v3.1-mix-preview-4096-OLMoE 0.1 \
 --checkpointing_steps epoch \
 --add_bos \
 --gradient_checkpointing
